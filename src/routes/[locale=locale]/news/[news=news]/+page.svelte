64abbfafd38e85d6243feade3725d54e88a92a5d
yogurt0713 <yogurt0713@gmail.com>
2025-02-26 11:22:10 +0900

    fixed #55

--- src/routes/[locale=locale]/news/[news=news]/+page.svelte
+++ src/routes/[locale=locale]/news/[news=news]/+page.svelte
@@ -1,13 +1,36 @@
 <script>
+  import { marked } from 'marked';
+  import { onDestroy } from 'svelte';
   import { page } from '$app/stores';
   import { formatDate } from '$lib/services/i18n';
   import { allNews } from '$lib/services/marketing/news';
   import { allPages } from '$lib/services/marketing/pages';
-  import { marked } from 'marked';
+
+  let parseBody = '';
+  let timeoutId;
 
   $: ({ locale = 'ja', news: slug } = $page.params);
   $: strings = allPages?.[locale]?.news;
   $: ({ title, date, cover, body, link } = allNews[locale][slug] || {});
+
+  $: if (body) {
+    const renderer = new marked.Renderer();
+
+    renderer.image = (href, text, imgtitle) =>
+      `<figure class="image-container"> <img src="${href}" alt="${text}" class="img-src" /> ${
+        title ? `<figcaption>${imgtitle}</figcaption>` : ''
+      } </figure>`;
+
+    parseBody = marked(body, { renderer });
+    console.log('parseBody updated', parseBody);
+  }
+
+  // コンポーネントが破棄されたときにクリーンアップ
+  onDestroy(() => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+  });
 </script>
 
 <section id="first" class="main">
@@ -19,7 +42,7 @@
       {/if}
       <section>
         <p>{formatDate(date, locale)}</p>
-        {@html marked.parse(body || '')}
+        {@html parseBody}
         {#if link}
           <p class="link"><a href={link}>{link}</a></p>
         {/if}
@@ -31,6 +54,26 @@
 <style lang="scss">
   .cover {
     width: 100%;
+  }
+
+  :global(.image-container) {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 1em 0;
+  }
+
+  :global(.img-src) {
+    display: block;
+    max-width: 100%;
+    height: auto;
+  }
+
+  :global(figcaption) {
+    font-size: 0.9em;
+    color: #555;
+    margin-top: 0.5em;
+    text-align: center;
   }
 
   .link {
